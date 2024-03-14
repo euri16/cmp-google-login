6a522388758facbb555cee38b26184fa421876f6
Eury Pérez <perez.eury16@gmail.com>
2024-03-15 00:46:33 +0100

    Showing user name on login

--- composeApp/src/commonMain/kotlin/dev/euryperez/loginsample/App.kt
+++ composeApp/src/commonMain/kotlin/dev/euryperez/loginsample/App.kt
@@ -3,13 +3,21 @@ package dev.euryperez.loginsample
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import dev.euryperez.loginsample.utils.compositionlocals.LocalResources
 import dev.euryperez.loginsample.signin.GoogleButton
 import dev.euryperez.loginsample.signin.models.AuthResponse
@@ -26,11 +34,21 @@ fun App() {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
+                var userName: String by remember { mutableStateOf("") }
+
                 GoogleButton(
                     onResponse = {
-                        println((it as? AuthResponse.Success)?.account?.profile?.email)
+                        (it as? AuthResponse.Success)?.account?.profile?.name?.let { name ->
+                            userName = name
+                        }
                     }
                 )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                if (userName.isNotEmpty()) {
+                    Text("Welcome $userName")
+                }
             }
         }
     }
