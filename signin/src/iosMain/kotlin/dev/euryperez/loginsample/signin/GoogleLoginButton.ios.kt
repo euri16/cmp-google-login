48f64acdb248405e2c19cc390712d4a30fc66ec1
Eury Pérez <perez.eury16@gmail.com>
2024-03-14 18:45:58 +0100

    Creating extensions to parse to GoogleAccount

--- signin/src/iosMain/kotlin/dev/euryperez/loginsample/signin/GoogleLoginButton.ios.kt
+++ signin/src/iosMain/kotlin/dev/euryperez/loginsample/signin/GoogleLoginButton.ios.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.interop.LocalUIViewController
 import cocoapods.GoogleSignIn.GIDSignIn
+import cocoapods.GoogleSignIn.GIDSignInResult
 import dev.euryperez.loginsample.signin.models.AuthResponse
 import dev.euryperez.loginsample.signin.models.GoogleAccount
 import dev.euryperez.loginsample.signin.models.Profile
@@ -29,28 +30,30 @@ private fun googleLogin(
     uiViewController: UIViewController,
     onLoggedIn: (AuthResponse) -> Unit
 ) {
-    GIDSignIn.sharedInstance.signInWithPresentingViewController(uiViewController) { user, error ->
-        if (user == null) {
+    GIDSignIn.sharedInstance.signInWithPresentingViewController(uiViewController) { result, error ->
+        if (result == null) {
             onLoggedIn(AuthResponse.Error(error?.fullErrorMessage ?: "Unknown error"))
             println("Error: $error")
             return@signInWithPresentingViewController
         }
 
-        val googleAccount = GoogleAccount(
-            idToken = user.user.idToken?.tokenString.orEmpty(),
-            accessToken = user.user.accessToken.tokenString,
-            profile = Profile(
-                name = user.user.profile?.name.orEmpty(),
-                familyName = user.user.profile?.familyName.orEmpty(),
-                givenName = user.user.profile?.givenName.orEmpty(),
-                email = user.user.profile?.email.orEmpty(),
-                picture = user.user.profile?.imageURLWithDimension(100u)?.absoluteString
-            ),
-        )
-
-        onLoggedIn(AuthResponse.Success(googleAccount))
+        onLoggedIn(AuthResponse.Success(result.toGoogleAccount))
     }
 }
+
+@OptIn(ExperimentalForeignApi::class)
+private val GIDSignInResult.toGoogleAccount: GoogleAccount
+    get() = GoogleAccount(
+        idToken = user.idToken?.tokenString.orEmpty(),
+        accessToken = user.accessToken.tokenString,
+        profile = Profile(
+            name = user.profile?.name.orEmpty(),
+            familyName = user.profile?.familyName.orEmpty(),
+            givenName = user.profile?.givenName.orEmpty(),
+            email = user.profile?.email.orEmpty(),
+            picture = user.profile?.imageURLWithDimension(100u)?.absoluteString
+        ),
+    )
 
 private val NSError.fullErrorMessage: String
     get() {
